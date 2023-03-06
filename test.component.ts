d3a48f645620a42616f2c9f1ddad9ee6410a8416
kurtasidao <112074223+kurtasidao@users.noreply.github.com>
2023-03-06 14:51:28 +0800

    Add files via upload

---
+++ test.component.ts
@@ -0,0 +1,15 @@
+export class TestComponent { 
+    showData($event: any){ 
+       console.log("button is clicked!"); if($event) { 
+          console.log($event.target); 
+          console.log($event.target.value); 
+          class TestComponent { 
+            userName:string = "Peter"; 
+            myCSSClass = "red"; 
+   applyCSSClass = false;
+   myColor = 'brown';
+         }
+       } 
+    } 
+ }
+ 
